508290daf79473456a878c397550c49f096fd6b2
Meschac Irung <meschacirung@gmail.com>
2024-07-15 23:01:28 +0200

    docs [avatar] : write component docs

---
+++ src/lib/references/avatar.ts
@@ -0,0 +1,19 @@
+export default {
+    head: {
+        cells: ['Prop', 'Type', 'Usage'],
+    },
+    rows: [
+        {
+            cells: ['size', `"xxs" | "xs" | "sm" | "base" | "lg" | "xl" | "xxl" | "xxxl"`, 'sz-[size]'],
+        },
+        {
+            cells: ['status', `"online" | "offline" | "busy" | "away"`, 'status-[status]'],
+        },
+        {
+            cells: ['fallbackVariant', `"solid" | "soft"`, 'fallback-[variant]'],
+        },
+        {
+            cells: ['fallback-intent', `"primary" | "secondary" | "accent" | "info" | "danger" | "warning" | "success" | "gray" | "neutral"`, 'intent-[intent]'],
+        },
+    ],
+}
