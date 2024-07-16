61b83f9d9c8383480f3d989304d1e877f2fd7971
Meschac Irung <meschacirung@gmail.com>
2024-07-16 11:46:58 +0200

    docs : reorder slugs

---
+++ src/lib/references/progress.ts
@@ -0,0 +1,13 @@
+export default {
+    head: {
+        cells: ['Prop', 'Type', 'Usage'],
+    },
+    rows: [
+        {
+            cells: ['size', `"xs" | "sm" | "md" | "lg"`, 'sz-[size]'],
+        },
+        {
+            cells: ['intent', `"primary" | "secondary" | "accent" | "info" | "danger" | "warning" | "success" | "gray" | "neutral"`, 'intent-[intent]'],
+        },
+    ],
+}
