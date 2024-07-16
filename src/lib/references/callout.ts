1e04ea6da64fb240793fbeb5d4973df1a1c6d90b
Meschac Irung <meschacirung@gmail.com>
2024-07-16 08:37:56 +0200

    docs [callout] : write component docs

---
+++ src/lib/references/callout.ts
@@ -0,0 +1,10 @@
+export default {
+    head: {
+        cells: ['Prop', 'Type', 'Usage'],
+    },
+    rows: [
+        {
+            cells: ['intent', `"primary" | "secondary" | "accent" | "info" | "danger" | "warning" | "success" | "gray"`, 'intent-[intent]'],
+        },
+    ],
+}
