4a7aa37397773fbf80fadacacc93af8dbfb616a2
Meschac Irung <meschacirung@gmail.com>
2024-07-16 08:45:36 +0200

    docs [card] : add component docs

---
+++ src/lib/references/card.ts
@@ -0,0 +1,10 @@
+export default {
+    head: {
+        cells: ['Prop', 'Type', 'Usage'],
+    },
+    rows: [
+        {
+            cells: ['variant', `"outlined" | "soft" | "mixed"`, 'variant-[variant]'],
+        },
+    ],
+}
