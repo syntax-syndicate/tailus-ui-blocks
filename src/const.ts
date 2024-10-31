4901c829ab6cd6d2992a3d6fd267219494630503
Meschac Irung <meschacirung@gmail.com>
2024-10-31 14:57:27 +0200

    add open graph image

--- src/const.ts
+++ src/const.ts
@@ -1,18 +1,18 @@
-export const SITE_TITLE = 'Tailus UI Html';
-export const SITE_DESCRIPTION = 'Highly customizable components for crafting modern, personalized websites and applications.';
-export const SITE_URL = 'https://tailus.io';
-export const SITE_IMAGE = '/og-cover.png';
-export const SITE_EMAIL = 'hello@tailus.io';
-export const SITE_BLOG = 'https://dev.to/tailus/';
-export const SITE_STORE = 'https://tailus.lemonsqueezy.com/';
-export const SITE_SOCIAL_TWITTER = 'https://twitter.com/tailus_ui';
-export const SITE_SOCIAL_FACEBOOK = 'https://web.facebook.com/tailusui';
-export const SITE_SOCIAL_GITHUB = 'https://github.com/tailus-ui/html.tailus.io';
-export const SITE_SOCIAL_LINKEDIN = 'https://www.linkedin.com/company/95954582';
-export const SITE_SOCIAL_YOUTUBE = 'https://www.youtube.com/@tailus-ui';
-export const SITE_SOCIAL_THREADS = 'https://www.threads.net/@irungmeschac';
+export const SITE_TITLE = 'Tailus UI Html'
+export const SITE_DESCRIPTION = 'Free up your time with pre-built classes that update site-wide automatically. Focus on building, not fixing.'
+export const SITE_URL = 'https://tailus.io'
+export const SITE_IMAGE = '/html-og.png'
+export const SITE_EMAIL = 'hello@tailus.io'
+export const SITE_BLOG = 'https://dev.to/tailus/'
+export const SITE_STORE = 'https://tailus.lemonsqueezy.com/'
+export const SITE_SOCIAL_TWITTER = 'https://twitter.com/tailus_ui'
+export const SITE_SOCIAL_FACEBOOK = 'https://web.facebook.com/tailusui'
+export const SITE_SOCIAL_GITHUB = 'https://github.com/tailus-ui/html.tailus.io'
+export const SITE_SOCIAL_LINKEDIN = 'https://www.linkedin.com/company/95954582'
+export const SITE_SOCIAL_YOUTUBE = 'https://www.youtube.com/@tailus-ui'
+export const SITE_SOCIAL_THREADS = 'https://www.threads.net/@irungmeschac'
 
-export const MESCHAC_AVATAR = 'https://avatars.githubusercontent.com/u/47919550?v=4';
-export const BERNARD_AVATAR = 'https://avatars.githubusercontent.com/u/31113941?v=4';
-export const THEO_AVATAR = 'https://avatars.githubusercontent.com/u/68236786?v=4';
-export const GLODIE_AVATAR = 'https://avatars.githubusercontent.com/u/99137927?v=4';
+export const MESCHAC_AVATAR = 'https://avatars.githubusercontent.com/u/47919550?v=4'
+export const BERNARD_AVATAR = 'https://avatars.githubusercontent.com/u/31113941?v=4'
+export const THEO_AVATAR = 'https://avatars.githubusercontent.com/u/68236786?v=4'
+export const GLODIE_AVATAR = 'https://avatars.githubusercontent.com/u/99137927?v=4'
